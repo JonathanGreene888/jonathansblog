6e20b77ea4eddc7bc52e9304b2674157f9fa793e
Jonathan Greene <Jonathangreene888@gmail.com>
2018-10-31 03:01:15 -0600

    Made the about us page dynamic

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
     def about
-    
+        @title = 'About us'; #how you initialize a variable in ruby
+        @content = 'This is the about us page';
     end
 end
