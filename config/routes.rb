c5c41cd6b21255828c1c02444ad04bed3544d1a2
Jonathan Greene <Jonathangreene888@gmail.com>
2018-10-31 14:22:24 -0600

    added in bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'posts#index' #creates base route
+  root 'posts#index', as: 'home' #creates base route
 
-  get 'about' => 'pages#about' #example of regular routes
+  get 'about' => 'pages#about', as: 'about' #example of regular routes
 
   resources :posts #matches http verbs
 
