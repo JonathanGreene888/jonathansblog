fc88d192e1c4a6d1dd6cde500a9ce232d7795bdd
Jonathan Greene <Jonathangreene888@gmail.com>
2018-10-31 02:52:00 -0600

    added a new about controller and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'posts#index'
+  root 'posts#index' #creates base route
+
+  get 'about' => 'pages#about' #example of regular routes
+
+
 end
