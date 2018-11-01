db8e22ea60cc959c3b772037426349ea81d4005e
Jonathan Greene <Jonathangreene888@gmail.com>
2018-11-01 14:39:36 -0600

    Added update ability to blog posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,6 +10,20 @@ class PostsController < ApplicationController
     def new 
         @post = Post.new
     end
+    
+    def edit
+        @post = Post.find(params[:id])
+    end
+
+    def update
+        @post = Post.find(params[:id])
+
+        if(@post.update(post_params))
+            redirect_to @post
+            else
+                render 'edit'
+            end
+    end
 
     def create
         @post = Post.new(post_params)
@@ -20,6 +34,8 @@ class PostsController < ApplicationController
             render 'new'
         end
     end
+
+    
 
     private def post_params
         params.require(:post).permit(:title, :body)
